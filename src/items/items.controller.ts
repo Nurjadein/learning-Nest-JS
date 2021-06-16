497fca3d56b9573a82d16db8a6d553b9ce9e3246
Nurjadein <nurjadeinabdulmorid@gmail.com>
2021-06-16 14:21:25 +0800

    GET and POST

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { create } from 'domain';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  @Get()
+  findAll(): string {
+    return 'u';
+  }
+  @Post()
+  create(): string {
+    return 'create item';
+  }
+
+}
