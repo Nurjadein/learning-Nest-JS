cf5198f50fed7da2f46182594ca69108001b43ff
Nurjadein <nurjadeinabdulmorid@gmail.com>
2021-08-15 23:46:14 +0800

    intro to setting up controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Get()
+  getUsers(): Array<any> {
+    return [{ id: 0 }];
+  }
+  @Get(':id')
+  getUsersById(@Param('id') id: string): any {
+    return { id };
+  }
+}
