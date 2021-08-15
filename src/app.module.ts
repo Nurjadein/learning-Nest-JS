670eb925bbf49b1940b10979478663d2ebc2f213
Nurjadein <nurjadeinabdulmorid@gmail.com>
2021-08-15 23:01:11 +0800

    initial commit 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsController } from './items/items.controller';
-import { ItemsService } from './items/items.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ItemsController],
-  providers: [AppService, ItemsService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
