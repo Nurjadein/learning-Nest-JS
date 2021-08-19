4d60381507552d6e264f98bb3db30db9ee0c37b3
Nurjadein <nurjadeinabdulmorid@gmail.com>
2021-08-19 17:03:15 +0800

    create function

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,21 @@
 import { UsersService } from './users.service';
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
   @Get()
-  getUsers(): any {
+  getUsers(): User[] {
     return this.usersService.findAll();
   }
   @Get(':id')
-  getUsersById(@Param('id') id: string): any {
+  getUsersById(@Param('id') id: string): User {
     return this.usersService.findById(id);
+  }
+  @Post()
+  createUser(@Body() body: CreateUserDto): User {
+    return this.usersService.createUser(body);
   }
 }
