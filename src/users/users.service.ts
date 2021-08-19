a160d8624929ad10f9756abbc2030300d626cfe9
Nurjadein <nurjadeinabdulmorid@gmail.com>
2021-08-19 17:03:30 +0800

    create function

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private users: any = [{ id: '0', name: 'Just Me' }];
+  private users: User[] = [{ id: '0', name: 'Just Me' }];
 
-  findAll() {
+  findAll(): User[] {
     return this.users;
   }
 
-  findById(userId: string) {
+  findById(userId: string): User {
     return this.users.find((user) => user.id === userId);
   }
-  createUser(name: string) {
-    const newUser = { id: Date.now(), name };
+  createUser(createUserDto: CreateUserDto): User {
+    const newUser = { id: String(Date.now()), ...createUserDto };
     this.users.push(newUser);
     return newUser;
   }
